5100aa575731f29ef7ab9ee73692783c344da269
Raghav Kapoor <raghavkapoor16947@gmail.com>
2026-03-17 00:57:28 +0530

    commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173' , "https://repolens.xyz"],
+    origin: ['http://localhost:5173' , "https://repolens.xyz" , "https://www.repolens.xyz"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true,
